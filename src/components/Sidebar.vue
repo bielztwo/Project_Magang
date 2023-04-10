902203ce47a77b8276878fc65bf2cb51220323d2
Marcello <octavyom@gmail.com>
2023-04-10 14:43:29 +0700

    Add new Feature SideBar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,223 @@
+<template>
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <aside :class="`${is_expanded ? 'is-expanded' : ''}`">
+        <div class="logo">
+            <img :src="logoURL" alt="Vue" />
+        </div>
+
+        <div class="menu-toggle-wrap">
+            <button class="menu-toggle" @click="ToggleMenu">
+                <span class="material-symbols-outlined">
+                keyboard_double_arrow_right
+                </span>
+            </button>
+        </div>
+
+        <h3>Menu</h3>
+        <div class="menu">
+            <router-link to="/" class="button">
+                <span class="material-symbols-outlined">
+                home_app_logo
+                </span>
+                <span class="text">Home</span>
+            </router-link>
+            <router-link to="/table" class="button">
+                <span class="material-symbols-outlined">
+                table_view
+                </span>
+                <span class="text">Table</span>
+            </router-link>
+            <router-link to="/about" class="button">
+                <span class="material-symbols-outlined">
+                help
+                </span>
+                <span class="text">About</span>
+            </router-link>
+        </div>
+
+        <div class="flex"></div>
+
+        <div class="menu">
+            <router-link to="/settings" class="button">
+                <span class="material-symbol-outlined">settings</span>
+                <span class="text">Settings</span>
+            </router-link>
+        </div>
+    </aside>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import logoURL from '../assets/logo.png'
+const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
+const ToggleMenu = () => {
+    is_expanded.value = !is_expanded.value
+    localStorage.setItem("is_expanded", is_expanded.value)
+}
+</script>
+
+<style lang="scss" scoped>
+
+aside {
+    display: flex;
+    flex-direction: column;
+    background-color: black;
+    color: white;
+    width: calc(2rem + 32px);
+    overflow: hidden;
+    min-height: 100vh;
+    padding: 1rem;
+    transition: 0.5s ease-out;
+
+    .flex {
+        flex: 1 1 0%;
+    }
+
+    .logo {
+        margin-bottom: 1rem;
+
+        img {
+            width: 2rem;
+        }
+    }
+    .menu-toggle-wrap {
+        display: flex;
+        justify-content: flex-end;
+        margin-bottom: 1rem;
+        position: relative;
+        top: 0;
+        transition: 0.5s ease-out;
+
+        .menu-toggle {
+            transition: 0.5s ease-out;
+
+            .material-symbol-outlined {
+                font-size: 2rem;
+                color: var(--light);
+                transition: 0.2s ease-out;
+            }
+
+            &:hover {
+                .material-symbol-outlined {
+                    color: var(--primary);
+                    transform: translateX(0.5rem);
+                }
+            }
+        }
+    }
+
+    h3,
+    .button .text {
+        opacity: 0;
+        transition: opacity 0.5s ease-out;
+    }
+
+    h3 {
+        color: var(--grey);
+        font-size: 0.875rem;
+        margin-bottom: 0.5rem;
+        text-transform: uppercase;
+    }
+
+    .menu {
+        margin: 0 -1rem;
+
+        .button {
+            display: flex;
+            align-items: center;
+            text-decoration: none;
+            transition: 0.5s ease-out;
+            padding: 0.5rem 1rem;
+
+            .material-symbol-outlined {
+                font-size: 2rem;
+                color: var(--light);
+                transition: 0.5s ease-out;
+            }
+
+            .text {
+                color: var(--light);
+                transition: 0.5s ease-out;
+            }
+
+            &:hover {
+                background-color: var(--dark-alt);
+
+                .material-symbol-outlined,
+                .text {
+                    color: var(--primary);
+                }
+            }
+
+            &.router-link-exact-active {
+                background-color: var(--dark-alt);
+                border-right: 5px solid var(--primary);
+
+                .material-symbol-outlined,
+                .text {
+                    color: var(--primary);
+                }
+            }
+        }
+    }
+
+    .footer {
+        opacity: 0;
+        transition: opacity 0.5s ease-out;
+
+        p {
+            font-size: 0.875rem;
+            color: grey;
+        }
+    }
+
+    &.is-expanded {
+        width: var(--sidebar-width);
+
+        .menu-toggle-wrap {
+            top: -3rem;
+
+            .menu-toggle {
+                transform: rotate(-180deg);
+            }
+        }
+
+        h3,
+        .button .text {
+            opacity: 1;
+        }
+
+        .button {
+            .material-symbol-outlined {
+                margin-right: 1rem;
+            }
+        }
+
+        .footer {
+            opacity: 0;
+        }
+    }
+
+    @media (max-width: 1024px) {
+        position: absolute;
+        z-index: 99;
+    }
+}
+</style>
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
